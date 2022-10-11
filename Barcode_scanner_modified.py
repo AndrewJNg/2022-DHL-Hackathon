5de279473c2d7a9d1a1b517ff79d4e7c9cbc8491
AndrewJNg <andrew.joseph.ng@gmail.com>
2022-10-12 00:41:33 +0800

    added a filter and printing system to only show when barcode changes

--- Barcode_scanner_modified.py
+++ Barcode_scanner_modified.py
@@ -8,16 +8,24 @@ from pyzbar import pyzbar
 '''url = "http://192.168.43.1:8080/video"    # uncomment when using phone camera through wifi
 cap = cv2.VideoCapture(url)'''  
 cap = cv2.VideoCapture(0)                   # use in built camera instead
+lastbarcode = 0
 
 while True:
 
-    
+   
+# setup display specs 
     ret, frame = cap.read()
-    frame = cv2.resize(frame, (0,0),fx = 0.50,fy=0.50)
+    # frame = cv2.resize(frame, (0,0),fx = 0.50,fy=0.50)
+    frame = cv2.resize(frame, (0,0),fx = 1,fy=1)
     
-    cv2.putText(frame,"Press q to close camera",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,100),1)
+# show instruction to close
+    cv2.putText(frame,"Q to exit",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,100),1)
+    # cv2.putText(frame,"Press q to close camera",(10,10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,255,100),1)
 
+
+# barcode processing
     barcodes = pyzbar.decode(frame)
+    # print(barcode)
     for barcode in barcodes:
         (x,y,w,h) = barcode.rect
         cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),1)
@@ -25,9 +33,24 @@ while True:
         barcodeType = barcode.type
         text = "Barcode: {}\n Type: {}".format(barcodeData,barcodeType)
         cv2.putText(frame,text,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1)
+
+        # barcode filter
+        isEqualLastBarcode = lastbarcode != barcodeData
+        contain_symbols = '?' in barcodeData 
+        if (isEqualLastBarcode & ~contain_symbols ):
+            print(barcodeData)
+            lastbarcode = barcodeData
+        # cv2.putText(frame,text,(x,y-10),cv2.FONT_HERSHEY_SIMPLEX,0.5,(0,0,255),1)
+
+# display
     cv2.imshow("Barcode Scanner",frame)
+
+    
+    # exit when q is pressed
+    # cv2.waitKey(1)
     if cv2.waitKey(1) == ord('q'):
         break
+
 if text:
     print(text)
 cap.release()
